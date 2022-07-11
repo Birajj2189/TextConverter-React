01fa31e06f5c7dd5dfa631717bee4c7cc547be99
Birajj2189 <birajj2001@gmail.com>
2022-07-11 22:13:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Textarea from './components/Textarea'; 
+import React,{useState} from 'react'
+import Alert from './components/Alert';
+
+// React router DOM -----------
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+// -----------------------------
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
+
 
 function App() {
+  const [mode , switchMode] = useState('light');
+  const [theme , switchTheme] = useState('warning');
+  const [alert , setAlert] = useState(null);
+
+  const showAlert = (message,type) => { 
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
+  }
+  const toggleMode = ()=>{
+    if(mode === "dark")
+    {
+      switchMode("light");
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+      showAlert("Light mode is enabled : ","success");
+    }
+    else{
+      switchMode("dark");
+      document.body.style.backgroundColor = ' rgb(10 9 8)';
+      document.body.style.color = 'white';
+      showAlert("Dark mode is enabled : ","success");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history} >
+      <Navbar theme = {mode} toggleMode={toggleMode} />
+      <Alert alert={alert} style={{position:'fixed'}}/>
+      <div className='container'>
+        <Routes>
+          <Route path="/about" element ={<About mode={mode}/>}/>
+          <Route path="/" element={<Textarea alert={showAlert} mode={mode} heading = "Enter your text here : "/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
